70e6a9a9aba6ad2774a6cf4344b6676e6ded7700
Hoog-v <hogeweyv@gmail.com>
2025-03-22 13:03:23 +0100

    first test run of pixi on linux

--- conda/build.sh
+++ conda/build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 set -eux
 
-# Package your pre-built ROS 2 node (from .pixi environment)
-mkdir -p $PREFIX/include
-mkdir -p $PREFIX/lib
-mkdir -p $PREFIX/share
-
-cp -r .pixi/envs/default/audio_tools/include/* $PREFIX/include/
-cp -r .pixi/envs/default/audio_tools/lib/* $PREFIX/lib/
-cp -r .pixi/envs/default/audio_tools/share/* $PREFIX/share/
+colcon build --merge-install --install-base=$CONDA_PREFIX
